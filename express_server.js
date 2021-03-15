8940cc8f0e7b619b912a76f70cdb3d7da7369d6c
Gabriel Silva <j.gabriel.888@gmail.com>
2021-03-15 21:46:09 +0000

    Implemented the new URL creation.

--- express_server.js
+++ express_server.js
@@ -12,51 +12,45 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
 function generateRandomString() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let randomString = "";
-  let randomIndex = 0
-  for (let i = 0; i < 6; i++) {
-    randomIndex = (Math.floor(Math.random() * 62));
-    randomString += characters[randomIndex];
-  }
-  return randomString;
+  let string = Math.random().toString(36).substring(7);
+  return string;
 };
 
 
 
 
-
+//Home page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+//URLS page
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 })
 
+//New URL Form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//New URL Creation
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 
+app.get("/u/:shortURL", (req, res) => {
+
+  res.redirect(`${urlDatabase.shortURL}`);
+});
+
+//URL show page
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req.params)
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
